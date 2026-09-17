3913d8de66e209ee497ef9e5221d591f01dad78c
RIMA-68 <forest0608h@gmail.com>
2026-09-17 22:01:32 +0900

    260917_01

---
+++ 260917_DAY9/01_260917.sql
@@ -0,0 +1,13 @@
+USE wconcept_db_260915;
+
+SHOW TABLES;
+
+SELECT * FROM blog_posts;
+SELECT * FROM brands;
+SELECT * FROM crawl_runs;
+SELECT * FROM product_rankings;
+SELECT * FROM product_snapshots;
+SELECT * FROM products;
+SELECT * FROM review_evaluations;
+SELECT * FROM review_images;
+SELECT * FROM reviews;
